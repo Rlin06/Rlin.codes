9599e8b701318290539c39bd7a95e59d45abcf91
Rlin06 <rlin@rlin.codes>
2016-02-25 04:37:42 -0500

    Flo Summary Written; Source Code Comment Added

--- public_html/sendemail.php
+++ public_html/sendemail.php
@@ -18,23 +18,3 @@ mail($to, $subject, $message, $headers);
 die;
 
 ?>
-
-
-
-<?php
-    if (isset($_POST["submit"])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
-        $from = $name;
-        $to = 'rlin@rlin.codes';
-        $subject = "[rlin.codes contact] $subject";
-        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-    if (mail ($to, $subject, $body, $from)) {
-        $result='<div class="alert alert-success">Thanks for reaching out!</div>';
-    } else {
-        $result='<div class="alert alert-danger">Sorry, there was an error. Please email me directly at rlin@rlin.codes</div>';
-    }
-    }
-?>
